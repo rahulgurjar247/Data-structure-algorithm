e0775b6b05fffadd2d65d2f8441952d21100467a
rahul <rahul@gmail.com>
2024-03-15 13:55:49 +0530

    add 1more update

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 int main()
 {
+    int x = 10;
+    char y = 'r';
+    float z = 23.4;
     cout << " hello rahul \n";
-    cout << "ha bhai khus \n";
+    cout << x <<" value of x \n";
+    cout << y <<" value of y \n";
+    cout << z <<" value of z \n";
 }
